ee33b679a9451346428025ff4db336305e2780dd
julichavez795 <julichavezestela26@gmail.com>
2025-10-23 09:54:43 -0300

    Create Tp objetos.py

---
+++ Tp objetos.py
@@ -0,0 +1,79 @@
+class componenteCPU:
+    listacomponentes= []
+    def __init__(self,componente,marca,cantidad,precio):
+        self.componente = str(componente)
+        self.marca = str(marca)
+        self.cantidad = int(cantidad)
+        self.precio = float(precio)
+        componenteCPU.listacomponentes.append(self)
+    def __str__(self):
+        return f"{self.componente} ({self.marca}) - Cantidad: {self.cantidad}, Precio: ${self.precio}"
+
+class computadora: 
+    def __init__(self,marca,modelo,listacomponentes):
+        self.marca= str(marca)
+        self.modelo= str(modelo)
+        self.listacomponentes= listacomponentes
+
+    def calcular_costo(self):
+        total= 0
+        for c in self.listacomponentes:
+            total += c.cantidad * c.precio
+        return total
+        
+    def calcular_precio_venta(self):
+        costo = self.calcular_costo()
+        if costo < 50000:
+            return costo + (costo * 0.40)
+        else:
+            return costo + (costo * 0.30)
+
+
+
+    def mostrar_informacion(self):
+        print("------ COMPUTADORA ------")
+        print(f"Marca: {self.marca}")
+        print(f"Modelo: {self.modelo}")
+        print("Componentes:")
+        for c in self.listacomponentes:
+            print("  -", c)
+
+        costo = self.calcular_costo()
+        precio_venta = self.calcular_precio_venta()
+
+        print(f" Costo total: ${costo}")
+        print(f"Precio de venta sugerido: ${precio_venta}")
+
+
+class CostoComputadora:
+    def main(self):
+        while True:
+            marca = input("Ingrese la marca de la computadora: ")
+            modelo = input("Ingrese el modelo de la computadora: ")
+
+            componentes = []
+            n = int(input("¿Cuántos componentes desea cargar? "))
+
+            for i in range(n):
+                print(f"Componente {i+1}")
+                comp = input("Nombre del componente: ")
+                marca_comp = input("Marca del componente: ")
+                cant = int(input("Cantidad: "))
+                precio = float(input("Precio unitario: "))
+
+                nuevo = componenteCPU(comp, marca_comp, cant, precio)
+                componentes.append(nuevo)
+
+            pc = computadora(marca, modelo, componentes)
+            pc.mostrar_informacion()
+
+            continuar = input("¿Desea evaluar otra computadora? (SI/NO): ").upper()
+            if continuar == "NO":
+                print("Programa finalizado.")
+                break
+
+
+# Ejecutar el programa principal
+if __name__ == "__main__":
+    programa = CostoComputadora()
+    programa.main()
