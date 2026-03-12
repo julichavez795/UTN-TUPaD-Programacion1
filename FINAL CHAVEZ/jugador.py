cf4a36c11e7bdfb83e78696545d8c89786619e51
julichavez795 <julichavezestela26@gmail.com>
2026-03-12 19:46:54 -0300

    Create jugador.py

---
+++ FINAL CHAVEZ/jugador.py
@@ -0,0 +1,6 @@
+class Jugador:
+    def __init__(self, numero, nombreCompleto, edad, puesto):
+        self.numero = int(numero)
+        self.nombreCompleto = nombreCompleto
+        self.edad = int(edad)
+        self.puesto = puesto
