114f499ad8cab6b117f0f03a41c643c00538e7fb
julichavez795 <julichavezestela26@gmail.com>
2026-03-12 19:48:51 -0300

    Create comparativaPelicula.py

---
+++ practicaa/comparativaPelicula.py
@@ -0,0 +1,7 @@
+class ComparativaPeliculas:
+    def __init__(self, p1, p2, puntaje1, puntaje2, ganador):
+        self.p1 = p1
+        self.p2 = p2
+        self.puntaje1 = puntaje1
+        self.puntaje2 = puntaje2
+        self.ganador = ganador
